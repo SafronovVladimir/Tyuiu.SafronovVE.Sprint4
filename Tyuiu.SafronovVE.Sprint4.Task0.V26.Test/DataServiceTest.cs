adbbdc70d9b7679e9d48ab47963cf1b45724a4dd
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-21 17:32:09 +0500

    Task0

---
+++ Tyuiu.SafronovVE.Sprint4.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SafronovVE.Sprint4.Task0.V26.Lib;
+namespace Tyuiu.SafronovVE.Sprint4.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+            int wait = 41;
+            int res = ds.GetSumOddArrEl(array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint4.Task0.V26/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.SafronovVE.Sprint4.Task0.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Сафронов В. Е. | ИБКСб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Сафронов Владимир Евгеньевич | ИБКСб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму          *");
+        Console.WriteLine("* нечетных элементов массива.  {9 ,3 ,7 ,1 ,5 ,5 ,3 ,2 ,1 ,7}             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] array = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+
+        Console.WriteLine("Исходный массив: ");
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма нечетных элементов: " + ds.GetSumOddArrEl(array));
+
+        Console.ReadKey();
+
+    }
+}
