b50aa8ebe2571a7cee5c33f7d062488beb170280
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-21 18:37:50 +0500

    Task1

---
+++ Tyuiu.SafronovVE.Sprint4.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SafronovVE.Sprint4.Task1.V7.Lib;
+namespace Tyuiu.SafronovVE.Sprint4.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[] { 2, 5, 3, 8, 2, 6, 2, 5, 5, 7, 4 };
+            var res = ds.Calculate(array);
+            var wait = 24;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint4.Task1.V7/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.SafronovVE.Sprint4.Task1.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Сафронов В. Е. | ИБКСб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Сафронов Владимир Евгеньевич | ИБКСб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+        Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму          *");
+        Console.WriteLine("* нечетных элементов массива.  {9 ,3 ,7 ,1 ,5 ,5 ,3 ,2 ,1 ,7}             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.Write("Введите длину массива: ");
+        int len = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        int[] array = new int[len];
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"Введите {i} элемент массива: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма четных элементов массива: " + ds.Calculate(array));
+
+        Console.ReadKey();
+    }
+}
