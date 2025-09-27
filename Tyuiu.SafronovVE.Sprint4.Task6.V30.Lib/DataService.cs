c9dd045654092e202e85f5f3f364bc23a38ec988
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-27 17:31:20 +0500

    Task6

---
+++ Tyuiu.SafronovVE.Sprint4.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SafronovVE.Sprint4.Task6.V30.Lib
+{
+    public class DataService : ISprint4Task6V30
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length > 5);
+            return mas;
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint4.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SafronovVE.Sprint4.Task6.V30.Lib;
+namespace Tyuiu.SafronovVE.Sprint4.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string[] array = new string[] { "Береза", "Дуб", "Клён", "Сосна", "Пихта", "Осина" };
+            var res = ds.Calculate(array);
+            string[] wait = { "Береза" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
