85b24e3ff14feec512d2a7e1fcb55627fb8f3f18
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-28 14:27:50 +0500

    Task7

---
+++ Tyuiu.SafronovVE.Sprint4.Task7.V28.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SafronovVE.Sprint4.Task7.V28.Lib
+{
+    public class DataService : ISprint4Task7V28
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] mtrx = new int[n, m];
+
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(index, 1));
+                    index++;
+                }
+            }
+            int prod = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mtrx[i, j] % 2 == 0)
+                    {
+                        prod *= mtrx[i, j];
+                    }
+                }
+                
+            }
+            return prod;
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Tyuiu.SafronovVE.Sprint4.Task7.V28.Lib;
+namespace Tyuiu.SafronovVE.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string str = "623351179845632";
+            int rows = 5;
+            int cols = 3;
+            int wait = 4608;
+            int res = ds.Calculate(rows, cols, str);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
